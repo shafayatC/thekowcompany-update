a4548211b0daf716d854fdfb39b87d4466788557
shafayat18 <shafayatplay@gmail.com>
2024-02-20 15:42:40 +0600

    thank you page buttin added

--- src/component/Thankyou/Thankyou.jsx
+++ src/component/Thankyou/Thankyou.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { IoCaretBackSharp } from "react-icons/io5";
 
 
 function Thankyou() {
+
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     navigate('/');
+  //   }, 5000);
+  // })
+
   return (
     <div className="my-20 container mx-auto">
       <div className="">
@@ -27,6 +39,9 @@ function Thankyou() {
           <br />
           <u>info@thekowcompany.com</u>
         </p>
+        <button onClick={() => navigate(-2)} className="bgButtonAnim m-auto mt-4 bg-[#7c9c30] flex px-8 py-3 items-center rounded-full text-white"> 
+          <span className="flex items-center gap-1"><IoCaretBackSharp /> Go Back</span>
+        </button>
       </div>
     </div>
   );
