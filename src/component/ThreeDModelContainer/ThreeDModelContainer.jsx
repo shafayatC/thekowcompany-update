7509fc954d7a9337a32357e42d109c854f15f42c
faizulhassanRien <faizulhassan.rien@gmail.com>
2024-06-06 19:40:43 +0600

    off 3d section

--- src/component/ThreeDModelContainer/ThreeDModelContainer.jsx
+++ src/component/ThreeDModelContainer/ThreeDModelContainer.jsx
@@ -12,17 +12,18 @@ const ThreeDModelContainer = () => {
   return (
     <>
       <div className="py-10 w-full bg-gray-200 flex items-center">
-        <div className="container mx-auto">
+        <div className="container mx-auto ">
           <div className="flex lg:flex-row flex-col gap-1 items-center justify-center">
-            <div className="flex flex-col gap-1 w-full  px-16">
-              <div>
+            <div className="flex flex-col md:flex-row justify-center justify-items-center items-center gap-5  md:gap-10 w-full  px-16">
+              <div className="mb-10">
                 <img
                   loading="lazy"
-                  className="w-[160px] h-[70px]"
+                  className="w-full h-full"
                   src="/img/Holosnap-Logo_H_Color.webp"
                 />
               </div>
 
+              <div className="flex flex-col justify-items-center md:pt-10 items-center">
               <p className="text-black text-justify">
                 Holosnap is an advanced AI-powered 3D asset creation tool that
                 seamlessly converts 2D images into intricate 3D point clouds.
@@ -33,14 +34,16 @@ const ThreeDModelContainer = () => {
                 and accurate 3D point clouds by identifying key features,
                 textures, and contours.
               </p>
-              <div className="flex justify-center mt-3 mb-5 md:mb-0">
+              <div className="flex justify-center mt-3 md:mt-6 mb-5 md:mb-0">
                 <Link aria-label="contact-us" to="/contact-us">
                   <button className="text-xs   md:text-sm font-semibold text-white px-6 py-2 bg-[#7C9C30] rounded-3xl">
                     Explore Holosnap
                   </button>
                 </Link>
               </div>
+              </div>
             </div>
+            
             {/* <div className='h-[320px] md:h-[500px] w-full md:w-[570px]'>
                             <Suspense fallback={<div className='flex justify-center  items-center'>
                                
@@ -51,7 +54,9 @@ const ThreeDModelContainer = () => {
                             
                         </div> */}
           </div>
+          
         </div>
+       
       </div>
     </>
   );
