7151ea4864b7ded41315569bbe3b0f5db9189dc1
faizulhassanRien <faizulhassan.rien@gmail.com>
2024-07-03 19:21:37 +0600

    add new photos for mobile view

--- src/component/Career/Career.jsx
+++ src/component/Career/Career.jsx
@@ -7,7 +7,7 @@ const Career = () => {
         alert("Thank you for submitting your CV. We will get back to you soon!")
     }
   return (
-    <div className="bg-[#F3F0F1] h-[100vh]">
+    <div className="career-bg h-[100vh]">
       <div className="container mx-auto">
         <div className="flex flex-col items-center ">
           <div className="w-[450px] mt-[100px]">
@@ -45,17 +45,17 @@ const Career = () => {
        <div className="w-[450px]"> 
             <div className="flex flex-col items-start">
                 <div className="flex justify-center items-center gap-2">
-                    <SlCalender className="text-[#92988D]"/>
+                    <SlCalender className="text-green-800"/>
                 <h1>APPLY BEFORE</h1>
                 </div>
-                <h1>31st July, 2024 </h1>
+                <h1 className="text-xs pl-5">31st July, 2024 </h1>
             </div>
             <div className="flex flex-col items-start">
             <div className="flex justify-center items-center gap-2">
-            <MdOutlineEmail className="text-[#92988D]"/> 
+            <MdOutlineEmail className="text-green-800 text-xl"/> 
             <h1>AT</h1>
             </div>
-            <h1>career@thekowcompany.com</h1>
+            <h1 className="text-xs pl-5">career@thekowcompany.com</h1>
             </div>
         </div>
 
