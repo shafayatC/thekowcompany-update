f4cd79c044b947ebae2a07dce42f865defbe6db2
shafayat18 <shafayatplay@gmail.com>
2024-09-12 12:10:48 +0600

    ignore quipments page

--- src/component3d/TechEquipments/TechEquipments.jsx
+++ src/component3d/TechEquipments/TechEquipments.jsx
@@ -1,3 +1,4 @@
+import { HeadProvider, Meta } from "react-head";
 import ScrolTop from "../../component/ScrolTop/ScrolTop";
 import Contact3D from "../Contact3D/Contact3D";
 import Footer3D from "../Footer3D/Footer3D";
@@ -8,14 +9,19 @@ import TEPage1 from "./TEPage1/TEPage1";
 
 const TechEquipments = () => {
     return (
-        <div>
-            <ScrolTop/>
-            <Navbar3D/>
-            <StartPage/>
-            <TEPage1/>
-            <Contact3D/>
-            <Footer3D/>
-        </div>
+        <>
+            <HeadProvider>
+                <Meta name="robots" content="noindex" />
+            </HeadProvider>
+            <div>
+                <ScrolTop />
+                <Navbar3D />
+                <StartPage />
+                <TEPage1 />
+                <Contact3D />
+                <Footer3D />
+            </div>
+        </>
     );
 };
 
