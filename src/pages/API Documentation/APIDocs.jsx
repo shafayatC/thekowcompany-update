1b5be2dbd98a74839077bbc17b4f2765aaeb37fd
faizulhassanRien <faizulhassan.rien@gmail.com>
2024-11-14 11:07:28 +0600

    add new api documentation

---
+++ src/pages/API Documentation/APIDocs.jsx
@@ -0,0 +1,17 @@
+import NavbarAPI from "../../componentAPI/NavbarAPI";
+
+
+const APIDocs = () => {
+    return (
+       <>
+        <div className="2xl:container mx-auto">
+            <div className="bg-[#f4f6f0] h-screen">
+                <NavbarAPI/>
+                
+            </div>
+        </div>
+       </>
+    );
+};
+
+export default APIDocs;
