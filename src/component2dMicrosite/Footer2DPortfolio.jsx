39d0edd64046b292d94d21267951ae205453babc
shafayat18 <shafayatplay@gmail.com>
2024-12-20 12:40:53 +0600

    2d portfolio start

---
+++ src/component2dMicrosite/Footer2DPortfolio.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer2DPortfolio = () => {
+    return (
+        <div className='2xl:container 2xl:mx-auto'>
+            <div className='bg-[#4AD991] px-16 py-[26px] rounded-t-[12px]'>
+                <div className='flex justify-between'>
+                    <Link to={'/contact-us'} className='text-4xl font-bold text-black'>Contact Us</Link>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Footer2DPortfolio;
