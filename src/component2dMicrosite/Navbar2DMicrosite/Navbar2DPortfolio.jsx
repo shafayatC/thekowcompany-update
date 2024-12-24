6bb78c9ce40e1727a02e145ac8e7411774b45320
faizulhassanRien <faizulhassan.rien@gmail.com>
2024-12-24 19:25:51 +0600

    update 2d portfolio responsive

--- src/component2dMicrosite/Navbar2DMicrosite/Navbar2DPortfolio.jsx
+++ src/component2dMicrosite/Navbar2DMicrosite/Navbar2DPortfolio.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Navbar2DPortfolio = () => {
     return (
-        <div className='bg-[#155331] py-4 px-6 rounded-b-[12px]'>
+        <div className='bg-[#155331] py-4 px-6 lg:rounded-b-[12px]'>
             <div className='container mx-auto'>
                 <div className='flex justify-between text-white'>
-                    <h3 className='text-2xl font-bold'>TKCL</h3>
-                    <h3 className='text-xl font-medium'>The KOW Company</h3>
-                    <button className='text-base font-normal py-2 px-4 border-white border-[2px] border-solid rounded'>Book a Call</button>
+                    <h3 className='text-[16px] lg:text-2xl font-bold'>TKCL</h3>
+                    <h3 className='text-[16px] lg:text-xl font-medium'>The KOW Company</h3>
+                    <button className='text-[12px] lg:text-base font-normal py-1 lg:py-2 px-4 border-white border-[2px] border-solid rounded'>Book a Call</button>
                 </div>
             </div>
         </div>
