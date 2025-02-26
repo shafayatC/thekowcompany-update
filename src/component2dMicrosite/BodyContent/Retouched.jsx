0491bf88e9ee1aa80f288048283896486308af2a
shafayat18 <shafayatplay@gmail.com>
2025-02-26 14:38:31 +0600

    merge perfectly

--- src/component2dMicrosite/BodyContent/Retouched.jsx
+++ src/component2dMicrosite/BodyContent/Retouched.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 
 const Retouched = () => {
-    return (
-        <div className='container mx-auto bg-white mt-9 '>
-             <div className="  bg-white py-[36px]  ">
+  return (
+    <div className='container mx-auto bg-white mt-9 '>
+      <div className="  bg-white py-[36px]  ">
         <h1 className="w-full text-center text-[20px] lg:text-[36px] font-bold lg:leading-10 text-[#000] py-[23px] ">
-        Try the Ai powered background replacement solution with Retouched.ai
+          Try the Ai powered background replacement solution with Retouched.ai
         </h1>
-        
+
       </div>
 
       <div>
-      <div className="pb-10">
-          
+        <div className="pb-10">
+
           <div className="w-full flex justify-center items-center ">
             <iframe
               className="bg-white h-[500px]  2xl:h-[720px] md:h-[700px]  w-full lg:w-[90%] border-[3px] lg:border-4 border-[#255646] rounded-xl"
               src="https://www.retouched.ai"
               title="Embedded Website"
               frameBorder="0"
-            
+
               allowFullScreen
             />
           </div>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Retouched;
